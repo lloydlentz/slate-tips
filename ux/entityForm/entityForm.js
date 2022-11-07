554515cd873575f3811cbf687557964b20e72503
Lloyd Lentz <cledwyn@gmail.com>
2022-11-07 17:35:08 -0600

    Added Delete Field Awareness.

--- ux/entityForm/entityForm.js
+++ ux/entityForm/entityForm.js
@@ -1,3 +1,12 @@
+/**
+ *  A little script to update Slate Form Fields for an entity value.  
+ *  See the writeup on https://knowledge.technolutions.com/hc/en-us/community/posts/10119257269659-Self-Aware-Entity-Form 
+ * 
+ * v1.1 - Added Check for Person Field and they ability to Delete
+ * Lloyd Lentz || https://ll-l-ll.com   
+ * ©2022, All Rights Ignored.  Use this for your own joy and gainz
+ * https://yapfa.club
+ */
 var entity
 var fields = []
 var elements = []
@@ -18,11 +27,46 @@ $('div[data-export]').each(function(){
 console.log(fields)
 
 var entguid = $('[data-export="entguid"] input').val()
+var personguid = $('[data-export="person"] input').val()
+
 $('[data-export="sys:entity:id:'+entity+'"] input').val(entguid)
 console.log('entguid; ', entguid)
 
+if(!(!entguid || !personguid)){
+    $('div.action').append('<button id="entDeleteButton">Delete</button><img id="imgDelete" />')
+
+    $('#entDeleteButton').click(function () {
+        if(confirm('Really DELETE this record?')){
+            $.ajax({
+                url: "/portal/ux?cmd=db_delete_entity",
+                method: "POST",
+                data: {
+                    cmd: 'db_delete_entity',
+                    entguid: entguid,
+                    record: personguid, 
+                } ,
+                beforeSend: function(){
+                   $('#imgDelete').attr("src","https://lloydlentz.github.io/slate-tips/ux/entityForm/loading-blue-bounce-20h.gif");
+                }, 
+                success: function (result) {
+                    $('#imgDelete').attr("src","");
+                    history.go(-1);
+                }, 
+                error: function () {
+                    alert("ERROR: It's possible you did not setup the delete method correctly.  Have your Slate Admin go check Lloyd's writup again. 😻")
+                    console.log("error");
+                }
+            });
+                  
+        }    
+
+    })
+}
 
 
+/**
+ * Assumes you use 
+ */
 fields.forEach(function (field) {
     $.ajax({
         url: "/portal/ux",
@@ -34,7 +78,7 @@ fields.forEach(function (field) {
         } ,
         beforeSend: function(){
             $('[data-export="sys:entity:field:'+field+'"] label').first().append("<img id='img--"+field+"' />");
-           $('#img--'+field).attr("src","https://lloydlentz.github.io/slate-tips/ux/entityForm/loading-blue-bounce-20h.gif");
+            $('#img--'+field).attr("src","https://lloydlentz.github.io/slate-tips/ux/entityForm/loading-blue-bounce-20h.gif");
         }, 
         success: function (result) {
             // $('#img-'+response+'-'+field).attr("src","");
