d01a7033b254ccb24534cf30c7046496e154d2b0
Lloyd Lentz <cledwyn@gmail.com>
2022-11-09 17:18:10 -0600

    Create invite.sql

---
+++ ux/shorturl/invite.sql
@@ -0,0 +1,18 @@
+--declare @invite VARCHAR(max) = '/shorturltest/0001'
+--declare @event varchar(max)
+declare @refid varchar(max)
+declare @person UNIQUEIDENTIFIER
+
+select @event = value from string_split(@invite,'/') order by getdate() offset 1 ROWS FETCH next 1 rows only
+select @refid = value from string_split(@invite,'/') order by getdate() offset 2 ROWS FETCH next 1 rows only
+select @person = id from [person] where ref = @refid
+
+select @event, @refid, @person
+
+select concat(
+    (select value from [config] where [key] = 'https')
+    , '/register/'
+    , @event
+    , '?person='
+    , @person
+)
