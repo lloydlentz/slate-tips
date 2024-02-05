b6f0c752cac96275e5199b3cd0d4dcb110170047
Lloyd Lentz <cledwyn@gmail.com>
2024-02-05 16:49:50 -0600

    Update material_download_url.sql

--- materials/material_download_url.sql
+++ materials/material_download_url.sql
@@ -1,14 +1,29 @@
-			concat((select 
-						(select [value] from [config] where ([key] = 'https'))
-					from (values (null)) subquery(x)
-					), '/apply/download.pdf?part=', (select 
-						CONCAT('stream:', dbo.toGuidString([stream]))
-					from (values (null)) subquery(x)
-					), '&h=', (select 
-						dbo.toGuidString(dbo.md5(convert(varbinary(max), (select 
-									CONCAT('stream:', dbo.toGuidString([stream]))
-								from (values (null)) subquery(x)
-								) + dbo.salt())))
-							
-					from (values (null)) subquery(x)
-					))
+concat((select 
+	(select [value] from [config] where ([key] = 'https'))
+	from (values (null)) subquery(x)
+	), '/apply/download.pdf?part=', (select 
+		CONCAT('stream:', dbo.toGuidString([stream]))
+	from (values (null)) subquery(x)
+	), '&h=', (select 
+		dbo.toGuidString(dbo.md5(convert(varbinary(max), (select 
+					CONCAT('stream:', dbo.toGuidString([stream]))
+				from (values (null)) subquery(x)
+				) + dbo.salt())))
+			
+	from (values (null)) subquery(x)
+))
+
+
+----- You can also use this in a CJ Formula Export
+
+concat( 
+        (select [value] from [config] where ([key] = 'https'))
+        , '/apply/download.pdf?part='
+        , 'stream:'
+        , dbo.toGuidString([stream])
+        , '&h='
+        , dbo.toGuidString(dbo.md5(convert(varbinary(max), (select 
+                        CONCAT('stream:', dbo.toGuidString([stream]))
+                    from (values (null)) subquery(x)
+                    ) + dbo.salt())))        
+)
