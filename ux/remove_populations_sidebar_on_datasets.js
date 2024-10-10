0b0db25e88d963fb828527aaaab4eb7deab3c95a
Lloyd Lentz <cledwyn@gmail.com>
2024-10-10 07:27:50 -0500

    Update remove_populations_sidebar_on_datasets.js

--- ux/remove_populations_sidebar_on_datasets.js
+++ ux/remove_populations_sidebar_on_datasets.js
@@ -5,3 +5,22 @@
     
     // Remove the second <col> from the <colgroup>
     $("table.sidebar colgroup col:nth-child(2)").remove();
+
+
+
+ //VER2
+      $(document).ready(function() {
+        // REMOVE the Populations Sidebar  //
+
+        // Remove the second <td> that contains the <h2>Populations</h2>
+        // $("table.sidebar td").filter(function() {
+        //     return $(this).find("h2").text() === "Populations";
+        // }).remove();
+        
+        // // Remove the second <col> from the <colgroup>
+        // $("table.sidebar colgroup col:nth-child(2)").remove();
+
+        // Actually, just resize it
+        $("table.sidebar colgroup col:nth-child(2)").css("width", "100px");
+
+      });
