19eb064f44cd0c4b1963b107c71c50d97549903e
Lloyd Lentz <cledwyn@gmail.com>
2024-11-05 13:20:27 -0600

    Update expandable_reader_forms.js

--- forms/expandable_reader_forms.js
+++ forms/expandable_reader_forms.js
@@ -1,55 +1,33 @@
 // Expand the window
-
 function expand() {
-
-jQuery('.reader_send_panel').animate({"width": "35%", "top": "27px"});
-
-jQuery('#expandme').text("Collapse");
-
-jQuery('#expandme').toggleClass('closed')
-
+    jQuery('.reader_send_panel').animate({ "width": "35%", "top": "27px" });
+    jQuery('#expandme').text("Collapse");
+    jQuery('#expandme').toggleClass('closed')
 }
 
 // Collapse the window
-
 function collapse() {
-
-jQuery('.reader_send_panel').animate({"width": "350px", "top": "27px"});
-
-jQuery('#expandme').text("Expand");
-
-jQuery('#expandme').toggleClass('closed')
-
+    jQuery('.reader_send_panel').animate({ "width": "350px", "top": "27px" });
+    jQuery('#expandme').text("Expand");
+    jQuery('#expandme').toggleClass('closed')
 }
 
 // Add the link
-
 jQuery('.reader_send_panel_content').prepend('<a class="closed" href="#" id="expandme" name="expandme">Expand</a>');
 
 // Allow the window to flow over the left side menu.
-
-jQuery('.reader_send_panel').css({"z-index": 2});
+jQuery('.reader_send_panel').css({ "z-index": 2 });
 
 // Comment out this line to not expand it by default when the review form opens.
-
 expand();
 
 // Add a click event to the expand link.
-
-jQuery('#expandme').click(function(e) {
-
-e.preventDefault();
-
-if (jQuery(this).hasClass('closed')) {
-
-expand();
-
-}
-
-else {
-
-collapse();
-
-}
-
+jQuery('#expandme').click(function (e) {
+    e.preventDefault();
+    if (jQuery(this).hasClass('closed')) {
+        expand();
+    }
+    else {
+        collapse();
+    }
 }); 
